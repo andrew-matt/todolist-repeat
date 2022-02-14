7f8f5d5fdb8487867e5599ffb6bed3e698e03892
andrew-matt <97734646+andrew-matt@users.noreply.github.com>
2022-02-14 16:10:48 +0300

    Update ControlButtons.tsx

--- ControlButtons.tsx
+++ ControlButtons.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import {FilterValuesType} from "./App";
 
-const ControlButtons = () => {
+type ControlButtonsType = {
+    changeFilter: (filter: FilterValuesType) => void
+}
+
+const ControlButtons = (props: ControlButtonsType) => {
     return (
         <span>
-            <button>All</button>
-            <button>Active</button>
-            <button>Completed</button>
+            <button onClick={() => {props.changeFilter("all")}}>All</button>
+            <button onClick={() => {props.changeFilter("active")}}>Active</button>
+            <button onClick={() => {props.changeFilter("completed")}}>Completed</button>
         </span>
     );
 };

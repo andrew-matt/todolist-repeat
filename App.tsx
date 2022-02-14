01946135b14ef4213b38a3ff27c493c24561f7ce
andrew-matt <97734646+andrew-matt@users.noreply.github.com>
2022-02-14 16:11:13 +0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -2,24 +2,47 @@ import React, {useState} from 'react';
 import './App.css';
 import Todolist, {TaskType} from "./Todolist";
 
+export type FilterValuesType = "all" | "active" | "completed"
+
 function App() {
     const [tasks, setTasks] = useState<Array<TaskType>>([
         {id: 1, title: "HTML", isDone: true},
-        {id: 2, title: "CSS", isDone: true},
+        {id: 2, title: "CSS", isDone: false},
         {id: 3, title: "JS/TS", isDone: true},
 ])
+
+    const [filter, setFilter] = useState<FilterValuesType>("all");
 
     const removeTask = (taskID: number) => {
         const filteredTasks = tasks.filter(task => task.id !== taskID)
         setTasks(filteredTasks)
     }
 
+    const changeFilter = (filter: FilterValuesType) => {
+        setFilter(filter)
+    }
+
+    const getFilteredTasksForRender = () => {
+        switch (filter) {
+            case "completed":
+                return tasks.filter(t => t.isDone === true)
+            case "active":
+                return tasks.filter(t => t.isDone === false)
+            default:
+                return tasks
+        }
+    }
+
+    const filteredTasksForRender = getFilteredTasksForRender ()
+
     return (
         <div className="App">
             <Todolist
                 title={"What to learn"}
-                tasks={tasks}
-                removeTask={removeTask}/>
+                tasks={filteredTasksForRender}
+                removeTask={removeTask}
+                changeFilter={changeFilter}
+            />
         </div>
     );
 }

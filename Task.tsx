781e559d51bd5637f28bd4223bcb0035d5122a55
andrew-matt <97734646+andrew-matt@users.noreply.github.com>
2022-02-14 13:12:59 +0300

    Update Task.tsx

--- Task.tsx
+++ Task.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import {TaskType} from "./Todolist";
 
-type TaskPropsType = TaskType
+type TaskPropsType = TaskType & {
+    removeTask: (taskID: number) => void
+}
+
 
 const Task = (props: TaskPropsType) => {
+    console.log(props.removeTask)
     return (
         <li>
             <input type="checkbox"
                    checked={props.isDone}/>
             <span>{props.title}</span>
+            <button onClick={() => {props.removeTask(props.id)}}>x</button>
         </li>
     );
 };

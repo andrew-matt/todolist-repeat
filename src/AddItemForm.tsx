3e07f11e9619b8845f7e45d70062e4c9bc249cca
andrew-matt <andrew.matt@ya.ru>
2022-05-23 18:31:31 +0300

    add React.memo and useCallback to fix addItemForm component

--- src/AddItemForm.tsx
+++ src/AddItemForm.tsx
@@ -1,4 +1,4 @@
-import React, {ChangeEvent, KeyboardEvent, useState} from 'react';
+import React, {ChangeEvent, KeyboardEvent, memo, useState} from 'react';
 import {Box, Button, FormControl, IconButton, TextField} from '@material-ui/core';
 import {AddBox} from '@material-ui/icons';
 
@@ -6,7 +6,8 @@ type AddItemFormPropsType = {
     addItem: (title: string) => void
 }
 
-export function AddItemForm(props: AddItemFormPropsType) {
+export const AddItemForm = memo((props: AddItemFormPropsType) => {
+    console.log("AddItemForm")
     let [title, setTitle] = useState("")
     let [error, setError] = useState<string | null>(null)
 
@@ -24,7 +25,7 @@ export function AddItemForm(props: AddItemFormPropsType) {
     }
 
     const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
-        setError(null);
+        if (error) setError(null);
         if (e.charCode === 13) {
             addItem();
         }
@@ -43,4 +44,4 @@ export function AddItemForm(props: AddItemFormPropsType) {
             <AddBox />
         </IconButton>
     </div>
-}
+})
